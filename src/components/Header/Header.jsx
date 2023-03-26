e4ba297b4ee2de633141d880e35be51c89b2f68f
abu-jafor <abujaforwd@yahoo.com>
2023-03-26 21:27:05 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../../images/Logo.svg";
+
+const Header = () => {
+  return (
+    <div className="w-full h-24 bg-slate-800 flex justify-between p-4 ">
+      <img src={logo} />
+      <div className="text-3xl space-x-4 mx-8 font-bold text-slate-100 align-middle  mt-4">
+        <a href="/shop">shop</a>
+        <a href="/order">order</a>
+        <a href="/inventory">inventory</a>
+        <a href="/login">login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
